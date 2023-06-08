235e3b243b061670d277a4e2ccae323ef9712410
DrStaf <dark-apple@nacious.net>
2023-06-07 23:00:14 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
+set -x
 mkdir -p result
 docker run --platform=linux/amd64 -v $(pwd)/result:/result -v $(pwd)/script:/script -v $(pwd)/patches:/patches -it build_libwebrtc_android ./script/runner.sh all
