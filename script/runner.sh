857774f1902a676571610a4b78a28a05142d674b
DrStaf <dark-apple@nacious.net>
2023-06-07 22:59:42 -0400

    Update runner.sh

--- script/runner.sh
+++ script/runner.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -x
 git clone --branch bugfix-depot_tools-issue-workaround https://github.com/lumin-leaper/com.unity.webrtc --depth=1
 pushd com.unity.webrtc
 mkdir -p out/Debug
