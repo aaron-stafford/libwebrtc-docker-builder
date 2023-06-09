4aae521b3259ab1fa3b5199d6055c7fc67e4646d
DrStaf <dark-apple@nacious.net>
2023-06-09 18:38:51 -0400

    Update run-debug.sh

--- run-debug.sh
+++ run-debug.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 mkdir -p result
-docker run --platform=linux/amd64 -v /result:/result -v $(pwd)/script:/script -v $(pwd)/patches:/patches -it build_libwebrtc_android
+docker run --platform=linux/amd64 -v $(pwd)/../result:/result -v $(pwd)/script:/script -v $(pwd)/patches:/patches -it build_libwebrtc_android
