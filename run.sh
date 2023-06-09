2c6ba92ad3a8fd1d962d7ddfe2e77b70db43ba22
DrStaf <dark-apple@nacious.net>
2023-06-09 18:03:01 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 mkdir -p result
-docker run --platform=linux/amd64 -v /result:/result -v $(pwd)/script:/script -v $(pwd)/patches:/patches -it build_libwebrtc_android --env WEBRTC_GIT_REPO=$LIBWEBRTC_REPO --env WEBRTC_GIT_REPO_BRANCH=$LIBWEBRTC_REPO_BRANCH ./script/runner.sh all
+docker run --env WEBRTC_GIT_REPO=$LIBWEBRTC_REPO --env WEBRTC_GIT_REPO_BRANCH=$LIBWEBRTC_REPO_BRANCH --platform=linux/amd64 -v /result:/result -v $(pwd)/script:/script -v $(pwd)/patches:/patches -it build_libwebrtc_android --env WEBRTC_GIT_REPO=$LIBWEBRTC_REPO --env WEBRTC_GIT_REPO_BRANCH=$LIBWEBRTC_REPO_BRANCH ./script/runner.sh all
