6118bbc261416e7093825516c8494a528bb8483a
DrStaf <dark-apple@nacious.net>
2023-06-09 16:48:01 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 mkdir -p result
-docker run --platform=linux/amd64 -v /result:/result -v $(pwd)/script:/script -v $(pwd)/patches:/patches -it build_libwebrtc_android ./script/runner.sh all
+docker run --platform=linux/amd64 -v /result:/result -v $(pwd)/script:/script -v $(pwd)/patches:/patches -it build_libwebrtc_android --env WEBRTC_GIT_REPO=https://github.com/aaron-stafford/com.unity.webrtc --env WEBRTC_GIT_REPO_BRANCH=issue-802-part-1 ./script/runner.sh all
