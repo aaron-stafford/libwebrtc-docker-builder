dc99549b367a0a02ac551dccfb3d1d6c0479d00b
DrStaf <dark-apple@nacious.net>
2023-06-09 16:57:48 -0400

    Update runner.sh

--- script/runner.sh
+++ script/runner.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 set -x
+echo Cloning repo: $WEBRTC_GIT_REPO_BRANCH
+echo Cloning fork: $WEBRTC_GIT_REPO
 git clone --branch $WEBRTC_GIT_REPO_BRANCH $WEBRTC_GIT_REPO --depth=1
 pushd com.unity.webrtc
 mkdir -p out/Debug
